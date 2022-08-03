f373b89a5b7b1b7f09b8cb4ef8861cfb10d5308d
devsargam <sargampoudel100@gmail.com>
2022-08-03 22:26:53 +0545

    refactor: convert messy main file to simple

--- src/index.js
+++ src/index.js
@@ -1,36 +1,4 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const ExtendedClient = require("./helpers/core/extendedClient");
 
-const getCommands = require("./helpers/getCommands");
-
-client.commands = new Collection();
-
-for (const command of getCommands()) {
-  client.commands.set(command.data.name, command);
-}
-
-client.on("ready", () => {
-  console.clear();
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
-
-client.login(process.env.BOT_TOKEN);
+const FakerBot = new ExtendedClient();
+FakerBot.login(process.env.BOT_TOKEN);
